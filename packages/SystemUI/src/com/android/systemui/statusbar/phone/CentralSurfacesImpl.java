90789fdd78e00b5108a624c66fed1eb55ca6dda6
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-04-29 07:16:32 +0800

    SystemUI: clean heap memory on screen off
    Change-Id: If1ed8a9a02c190ba9fb056c093c21b8b91c85161

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -61,6 +61,7 @@ import android.os.BatteryManager;
 import android.os.Binder;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 import android.os.PowerManager;
 import android.os.RemoteException;
 import android.os.ServiceManager;
@@ -437,6 +438,8 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces, Medi
     private final NotificationsController mNotificationsController;
     private final StatusBarSignalPolicy mStatusBarSignalPolicy;
     private final StatusBarHideIconsForBouncerManager mStatusBarHideIconsForBouncerManager;
+    
+    private final Handler mHandler = new Handler(Looper.getMainLooper());
 
     /** Controller for the Shade. */
     private final ShadeSurface mShadeSurface;
@@ -2660,6 +2663,8 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces, Medi
             DejankUtils.stopDetectingBlockingIpcs(tag);
             com.android.systemui.util.ScrimUtils.getInstance(mContext).onScreenStateChange();
             doCpuStandbyOptimization(true);
+            // make sure we do garbage collection at screen off but delay it to avoid black wallpaper
+            mHandler.postDelayed(mSystemUiGcOpt, 1000);
         }
 
         @Override
@@ -2727,6 +2732,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces, Medi
             });
             DejankUtils.stopDetectingBlockingIpcs(tag);
             doCpuStandbyOptimization(false);
+            mHandler.removeCallbacks(mSystemUiGcOpt);
         }
 
         /**
@@ -2815,6 +2821,16 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces, Medi
                chargePlug == BatteryManager.BATTERY_PLUGGED_USB ||
                chargePlug == BatteryManager.BATTERY_PLUGGED_WIRELESS;
     }
+
+    private final Runnable mSystemUiGcOpt = new Runnable() {
+        @Override
+        public void run() {
+            System.gc();
+            System.runFinalization();
+            System.gc();
+            Log.v("GcOpt", "performing garbage collection for SystemUI");
+        }
+    };
 
     /**
      * We need to disable touch events because these might
